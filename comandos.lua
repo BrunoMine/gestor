6731bdf89281e4c52aaa329f652cbe4360cf38c6
BrunoMine <borgesdossantosbruno@gmail.com>
2018-12-27 20:20:16 -0200

    adaptação para mod sfinv_menu

--- comandos.lua
+++ comandos.lua
@@ -12,7 +12,20 @@ minetest.register_chatcommand("gestor", {
 	func = function(name)
 		local player = minetest.get_player_by_name(name)
 		player:set_attribute("gestor_aba", "inicio")
-		minetest.after(1, gestor.menu_principal, name, true)
+		minetest.after(0.1, gestor.menu_principal, name, true)
 	end
 })
 
+ -- Menu de acesso simples
+if sfinv_menu then
+	sfinv_menu.registrar_botao("gestor:painel", {
+		titulo = "Gestor",
+		icon = "gestor.png",
+		privs = {server=true},
+		func = function(player)
+			local name = player:get_player_name()
+			player:set_attribute("gestor_aba", "inicio")
+			minetest.after(0.1, gestor.menu_principal, name, true)
+		end,
+	})
+end
